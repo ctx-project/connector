5960793a2a1f844f9735d7ce16a4e70afeb3564d
andrei <bulk.ba@gmail.com>
2017-07-10 20:52:27 +0300

    basic ops. no live

---
+++ index.js
@@ -0,0 +1,60 @@
+var l = function(o) {console.log(o); return o;}
+if(require) var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+module.exports = CtxConnection;
+
+function CtxConnection(base, user) {
+	this.base = base;
+	this.user = user;
+	this.root = new CtxContext(this, '');
+}
+
+CtxConnection.prototype.getUrl = function() {
+	return 'http://' + this.base + '/ctx/' + this.user;	
+}
+
+CtxConnection.prototype.getText = function() {
+	return '';	
+}
+
+function CtxContext(parent, text) {
+	this.parent = parent;
+	this.setText(text);
+}
+
+CtxContext.prototype.setText = function(text) {
+	this.text = (text || '').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+CtxContext.prototype.getUrl = function() {
+	return this.parent.getUrl();
+}
+
+CtxContext.prototype.getText = function() {
+	return this.parent.getText() + ' ' + this.text;
+}
+
+CtxContext.prototype.get = function(text, cb) {
+	this.ajax(this.getUrl() + '/get/' + encodeURIComponent(this.getText() + ' ' + text), cb);
+}
+
+CtxContext.prototype.put = function(text, cb) {
+	this.ajax(this.getUrl() + '/put/' + encodeURIComponent(this.getText() + ' ' + text), cb);
+}
+
+CtxContext.prototype.sub = function(text) {
+	return new CtxContext(this, text);
+}
+
+CtxContext.prototype.ajax = function(url, cb) {
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('GET', url, true);
+	
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4)
+			cb(xhr.responseText);
+	}
+	
+	xhr.send();
+}
