cd9e654697700d293034da7a41aba3c7d2a1fc83
andrei <bulk.ba@gmail.com>
2017-08-15 21:18:19 +0300

    drop root; conn has sub

--- index.js
+++ index.js
@@ -9,44 +9,62 @@ module.exports = CtxConnection;
 function CtxConnection(base, user) {
 	this.base = base;
 	this.user = user;
-	this.root = new CtxContext(this, '');
+	this.depth = -1;
 }
 
 CtxConnection.prototype.getUrl = function() {
 	return (this.base.startsWith('http') ? '' : 'http://') + this.base + '/ctx/' + this.user;	
 }
 
-CtxConnection.prototype.getText = function() {
+CtxConnection.prototype.getQuery = function() {
 	return '';	
 }
 
-function CtxContext(parent, text) {
-	this.parent = parent;
-	this.setText(text);
+CtxConnection.prototype.getPath = function() {
+	return [];	
 }
 
-CtxContext.prototype.setText = function(text) {
-	this.text = (text || '').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+CtxConnection.prototype.sub = function(query) {
+	return new CtxContext(this, query);
+}
+
+function CtxContext(parent, query) {
+	this.parent = parent;
+	this.depth = parent.depth + 1;
+	this.setQuery(query);
+}
+
+CtxContext.prototype.setQuery = function(query) {
+	this.query = (query || '').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	return this;
+}
+
+CtxContext.prototype.getPath = function() {
+	var p = this.parent.getPath();
+	p.push(this.query);
+	return p;
 }
 
 CtxContext.prototype.getUrl = function() {
 	return this.parent.getUrl();
 }
 
-CtxContext.prototype.getText = function() {
-	return this.parent.getText() + ' ' + this.text;
+CtxContext.prototype.getQuery = function() {
+	return this.parent.getQuery() + ' ' + this.query;
 }
 
-CtxContext.prototype.get = function(text, cb) {
-	this.ajax(this.getUrl() + '/get/' + encodeURIComponent(this.getText() + ' ' + text), cb, this);
+CtxContext.prototype.get = function(query, cb) {
+	this.ajax(this.getUrl() + '/get/' + encodeURIComponent(this.getQuery() + ' ' + query), cb, this);
+	return this;
 }
 
-CtxContext.prototype.put = function(text, cb) {
-	this.ajax(this.getUrl() + '/put/' + encodeURIComponent(this.getText() + ' ' + text), cb, this);
+CtxContext.prototype.put = function(query, cb) {
+	this.ajax(this.getUrl() + '/put/' + encodeURIComponent(this.getQuery() + ' ' + query), cb, this);
+	return this;
 }
 
-CtxContext.prototype.sub = function(text) {
-	return new CtxContext(this, text);
+CtxContext.prototype.sub = function(query) {
+	return new CtxContext(this, query);
 }
 
 CtxContext.prototype.ajax = function(url, cb, ctx) {
